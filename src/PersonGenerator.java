a735d5eb5a3b6bf3ef73a5650823b3c9af68bf56
Alex <baloghaj@mail.uc.edu>
2023-01-12 16:55:19 -0500

    User Input for ArrayList

--- src/PersonGenerator.java
+++ src/PersonGenerator.java
@@ -1,9 +1,44 @@
+import java.util.ArrayList;
+import java.util.Scanner;
 
-
+/**
+ *
+ * @author Alex Balogh
+ */
 public class PersonGenerator {
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args){
 
+        Scanner in = new Scanner(System.in);
+        String idString;
+        String firstName;
+        String lastName;
+        String inputTitle;
+        int yearOfBirth;
+        String yearOfBirthST;
+        String stringPerson;
+        boolean doContinue;
+        ArrayList<String> PersonList = new ArrayList<>();
+
+        do
+        {
+            idString = SafeInput.getNonZeroLenString(in, "Enter Id");
+            firstName = SafeInput.getNonZeroLenString(in, "Input first name");
+            lastName = SafeInput.getNonZeroLenString(in, "Input last name");
+            inputTitle = SafeInput.getNonZeroLenString(in, "Input title");
+            yearOfBirth = SafeInput.getInt(in, "Input year of birth");
+
+            yearOfBirthST = Integer.toString(yearOfBirth);
+
+            stringPerson = String.join(", ", idString, firstName, lastName, inputTitle, yearOfBirthST);
+
+            PersonList.add(stringPerson);
+
+            doContinue = SafeInput.getYNConfirm(in, "Do you want to continue");
+
+            System.out.println(PersonList);
+            System.out.println(doContinue);
+
+        } while (doContinue);
     }
 }
